962855436952701502af2cf2757e83cbc0cc7235
João Paulo <j.paaulinho@gmail.com>
2020-05-25 20:25:45 -0300

    adittion of two new libs, one for detecting the OS of the user and another to go fullscreen on mobile (to not break the website structure)

--- src/containers/PromotionalPage/index.js
+++ src/containers/PromotionalPage/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import UAParser from 'ua-parser-js';
 import ReactPageScroller from 'react-page-scroller';
+import Fullscreen from "react-full-screen";
 
 import HomeSection from './sections/HomeSection';
 import CollectionsSection from './sections/CollectionsSection';
@@ -8,17 +10,27 @@ import ContactSection from './sections/ContactSection';
 
 import LanguageSwitcher from './components/LanguageSwitcher';
 import Pagination from './components/Pagination';
+import MobileFullScreen from './components/MobileFullscreen';
 
 import BearLogo from '../../assets/svgs/Bear.svg';
 
 import './styles.scss';
 
 class PromotionalPage extends Component {
-  
+
+  componentDidMount() {
+    let system = new UAParser();
+    this.setState({
+      device: system.getDevice().type
+    });
+  }
+
   state = {
     currentPage: 0,
     currentLanguage: 'english',
-    seasonsSection: 0
+    seasonsSection: 0,
+    device: '',
+    isFull: false
   }
 
   handlePageChange = number => {
@@ -31,7 +43,7 @@ class PromotionalPage extends Component {
 
   seasonsSectionHandler = (number) => {
     let newSection = number + this.state.seasonsSection;
-    
+
     if (newSection < 0) {
       this.setState({ seasonsSection: 0 });
     }
@@ -44,27 +56,32 @@ class PromotionalPage extends Component {
   }
 
   render() {
+    let example = this.state.device === 'mobile' ?
+      <MobileFullScreen language={this.state.currentLanguage} hide={this.state.isFull} setFull={() => this.setState({isFull: true})} /> : null
     return (
       <>
-      <img className="BearLogo" src={BearLogo} alt="Bear Logo"
-        style={{opacity: this.state.currentPage === 1 ? 1 : this.state.currentPage === 2 ? 1 : 0}}/>
-        <Pagination
-          handleSection={(number) => this.seasonsSectionHandler(number)}
-          currentPage={this.state.currentPage}
-          section={(number) => this.handlePageChange(number)}
-          currentSection={this.state.seasonsSection} />
-        <LanguageSwitcher
-          language={this.state.currentLanguage}
-          languageChanger={this.handleLanguageChange} />
-        <ReactPageScroller
-          pageOnChange={this.handlePageChange}
-          customPageNumber={this.state.currentPage}
-        >
-          <HomeSection season={this.state.seasonsSection} currentPage={this.state.currentPage} language={this.state.currentLanguage} />
-          <SeasonsSection season={this.state.seasonsSection}  currentSection={this.state.seasonsSection} currentPage={this.state.currentPage} language={this.state.currentLanguage} />
-          <CollectionsSection season={this.state.seasonsSection}  currentPage={this.state.currentPage} language={this.state.currentLanguage} />
-          <ContactSection season={this.state.seasonsSection}  currentPage={this.state.currentPage} language={this.state.currentLanguage} />
-        </ReactPageScroller>
+        <Fullscreen
+          enabled={this.state.isFull}>
+          {example}
+          <img className="BearLogo" src={BearLogo} alt="Bear Logo"
+            style={{ opacity: this.state.currentPage === 1 ? 1 : this.state.currentPage === 2 ? 1 : 0 }} />
+          <Pagination
+            currentPage={this.state.currentPage}
+            section={(number) => this.handlePageChange(number)}
+            currentSection={this.state.seasonsSection} />
+          <LanguageSwitcher
+            language={this.state.currentLanguage}
+            languageChanger={this.handleLanguageChange} />
+          <ReactPageScroller
+            pageOnChange={this.handlePageChange}
+            customPageNumber={this.state.currentPage}
+          >
+            <HomeSection season={this.state.seasonsSection} currentPage={this.state.currentPage} language={this.state.currentLanguage} />
+            <SeasonsSection handleSection={(number) => this.seasonsSectionHandler(number)} season={this.state.seasonsSection} currentSection={this.state.seasonsSection} currentPage={this.state.currentPage} language={this.state.currentLanguage} />
+            <CollectionsSection season={this.state.seasonsSection} currentPage={this.state.currentPage} language={this.state.currentLanguage} />
+            <ContactSection season={this.state.seasonsSection} currentPage={this.state.currentPage} language={this.state.currentLanguage} />
+          </ReactPageScroller>
+        </Fullscreen>
       </>
     )
   }
