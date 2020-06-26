991ce1cd9edee59c8af01bec3ab3dfb5a160bcae
João Paulo <j.paaulinho@gmail.com>
2020-06-25 22:15:37 -0300

    added the multi-languages file

---
+++ src/multiLanguages/languages/english.js
@@ -0,0 +1,89 @@
+let english = {
+  seasons: {
+    winter: {
+      title: "Winter",
+      subTitle: "Say Hello to the cold days!",
+
+      text1: {
+        h1: "Lorem Ipsum",
+        h2: "example lotem damet asbterco limtel.",
+      },
+
+      text2: {
+        h1: "Lorem Ipsum",
+        h2: "example lotem damet asbterco limtel.",
+      },
+    },
+
+    spring: {
+      title: "Spring",
+      subTitle: "The Love grows here!",
+
+      text1: {
+        h1: "Lorem Ipsum",
+        h2: "example lotem damet asbterco limtel.",
+      },
+
+      text2: {
+        h1: "Lorem Ipsum",
+        h2: "example lotem damet asbterco limtel.",
+      },
+    },
+
+    autumn: {
+      title: "Autumn",
+      subTitle: "The Season to fall in Love.",
+
+      text1: {
+        h1: "Lorem Ipsum",
+        h2: "example lotem damet asbterco limtel.",
+      },
+
+      text2: {
+        h1: "Lorem Ipsum",
+        h2: "example lotem damet asbterco limtel.",
+      },
+    },
+
+    summer: {
+      title: "Summer",
+      subTitle: "Beach time!!",
+
+      text1: {
+        h1: "Lorem Ipsum",
+        h2: "example lotem damet asbterco limtel.",
+      },
+
+      text2: {
+        h1: "Lorem Ipsum",
+        h2: "example lotem damet asbterco limtel.",
+      },
+    },
+  },
+
+  homeSection: {
+    title: "The Chillest fictional merch on the World!",
+  },
+
+  collectionsSection: {
+    title: "For all the Occasions",
+
+    cards: {
+      card1: "Casual choise",
+      card2: "Sunday morning",
+      card3: "Morning wakeup",
+      card4: "Workout time!",
+      card5: "Winter Collection",
+      card6: "Player 1 Start!",
+    },
+  },
+
+  contactSection: {
+    title: "What are you waiting for?!",
+    button: "Visit our Website",
+    newsletter: "Subscribe to our newsletter",
+    social: "Follow us on Social Media",
+  },
+};
+
+export default english;
---
+++ src/multiLanguages/languages/portuguese.js
@@ -0,0 +1,93 @@
+let portuguese = {
+
+  seasons: {
+
+    winter: {
+      title: 'Inverno',
+      subTitle: 'Diga Olá para os dias frios',
+
+      text1: {
+        h1: 'Lorem Ipsum',
+        h2: 'example lotem damet asbterco limtel.'
+      },
+
+      text2: {
+        h1: 'Lorem Ipsum',
+        h2: 'example lotem damet asbterco limtel.'
+      }
+    },
+
+    spring: {
+      title: 'Primavera',
+      subTitle: 'Onde o amor começa a florescer',
+
+      text1: {
+        h1: 'Lorem Ipsum',
+        h2: 'example lotem damet asbterco limtel.'
+      },
+
+      text2: {
+        h1: 'Lorem Ipsum',
+        h2: 'example lotem damet asbterco limtel.'
+      }
+    },
+
+    autumn: {
+      title: 'Outono',
+      subTitle: 'A Estação para cair de amores.',
+
+      text1: {
+        h1: 'Lorem Ipsum',
+        h2: 'example lotem damet asbterco limtel.'
+      },
+
+      text2: {
+        h1: 'Lorem Ipsum',
+        h2: 'example lotem damet asbterco limtel.'
+      }
+    },
+
+    summer: {
+      title: 'Verão',
+      subTitle: 'Hora da Praia!!',
+
+      text1: {
+        h1: 'Lorem Ipsum',
+        h2: 'example lotem damet asbterco limtel.'
+      },
+
+      text2: {
+        h1: 'Lorem Ipsum',
+        h2: 'example lotem damet asbterco limtel.'
+      }
+    },
+
+  },
+
+  homeSection: {
+    title: 'A Marca de roupas mais tranquila do Mundo!'
+  },
+
+  collectionsSection: {
+    title: 'Para todas as Ocasiões',
+
+    cards: {
+      card1: 'Escolha casual',
+      card2: 'Doming de manhã',
+      card3: 'Café da manhã',
+      card4: 'Hora de suar!',
+      card5: 'Colecão de inverno',
+      card6: 'Jogador Nº 1',
+    }
+  },
+
+  contactSection: {
+    title: 'O que você está esperando?!',
+    button: 'Visite nosso Website',
+    newsletter: 'Assine a nossa newsletter',
+    social: 'Nos siga nas Redes Sociais'
+  }
+
+}
+
+export default portuguese;
